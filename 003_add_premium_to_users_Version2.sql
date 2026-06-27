dd646b7434e2749fc39a8634df8115592aefe78f
PARTH-CODE2012 <poojagoswami99107114@gmail.com>
2026-06-27 19:33:35 +0530

    Delete 003_add_premium_to_users_Version2.sql

--- 003_add_premium_to_users_Version2.sql
+++
@@ -1,4 +0,0 @@
--- Add premium columns to users table
-ALTER TABLE users
-  ADD COLUMN IF NOT EXISTS is_premium BOOLEAN DEFAULT FALSE,
-  ADD COLUMN IF NOT EXISTS premium_expires TIMESTAMP WITH TIME ZONE NULL;
